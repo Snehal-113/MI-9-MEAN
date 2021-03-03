760de722dbd936cc1038e81836e71b6cec83bcac
shubhangi <shubhangimamulkar7@gmail.com>
2021-03-03 13:35:48 +0530

    Added React Assignments

---
+++ Assignments/REACT/Shubhangi_React/Shubhangi_A3.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import ReactDom from "react-dom";
+class Greeting extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is class component</h1>
+        <h1>Good Day</h1>
+        <h2>All Of You</h2>
+      </div>
+    );
+  }
+}
+
---
+++ Assignments/REACT/Shubhangi_React/Shubhangi_A6.js
@@ -0,0 +1,25 @@
+function ShowTime() {
+  let localtime = new Date().toLocaleTimeString();
+  const element = (
+    <div>
+      <h1>heej</h1>
+      <h1>time is,{localtime}</h1>
+    </div>
+  );
+ ReactDom.render(element,document.getElementById("root"));
+}
+setInterval(ShowTime,1000);
+
+function ShowTime() {
+  let localtime = new Date().toLocaleTimeString();
+  const element = (
+    <div>
+      <h1>This is for time interval</h1>
+      <h1>Hello </h1>
+      <h1>time is {localtime}</h1>
+    </div>
+  );
+ ReactDom.render(element,document.getElementById("root"));
+}
+setInterval(ShowTime,1000);
+
