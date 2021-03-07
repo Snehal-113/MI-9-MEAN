0fd92e517c7126cff63af8846317699a2d3f1593
Rajashre07 <rajashree.yeole@gmail.com>
2021-03-07 19:57:45 +0530

    Changes done

---
+++ Assignments/REACT/Rajashree/UpdateRajashreeAssignment2.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDom from "react-dom";
+ class ShowTime extends React.Component{
+   constructor(props){
+    super (props)
+    this.state = {date : new Date()}
+   };
+  render()
+  {
+    const Localtime = this.state.date.toLocaleTimeString();
+    return(
+      <div>
+        <h1> time with class</h1>
+        <h1> The Time is{Localtime}</h1>
+      </div>
+    );
+   }
+  }
+     const etime = <ShowTime />
+     ReactDom.render(etime,document.getElementById("root"));
