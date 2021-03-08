234cfad354272d2e01da207c98af41b6f0a88e92
Sachin <jadhavsachin9824@gmail.com>
2021-03-08 21:59:03 +0530

    react Assignment added

---
+++ Assignments/React/Sachin/Sachin_Q.12.js
@@ -0,0 +1,35 @@
+class Time extends React.Component{
+    constructor(props){
+      super(props);
+      this.state={date: new Date()};
+      console.log(new Date());
+    }
+    componentDidMount()
+    {
+        this.id=setInterval(()=>this.setTimer(),1000);
+    }
+    componentWillUnmount()
+    {
+        clearInterval(this.id);
+      
+    }
+    setTimer()
+    {
+        this.setState({date:new Date()});
+    }
+    render()
+    {
+    let loctime=this.state.date.toLocaleTimeString();
+  return (
+    <div>
+      <h2>Hello everyone</h2>
+      <h3>How it is {loctime} 0'clock</h3>
+    </div>
+  );  
+    
+  }  
+  }
+    const time_element=<Time  />
+    ReactDOM.render(time_element, document.getElementById("root"));
+     
+  
