13db0f13168e1a1a95f5c3252e3fbb25241ec72e
preeti <preetipie22@gmail.com>
2021-03-18 10:45:58 +0530

    added assignment 16

---
+++ Assignments/REACT/Preeti/preeti_a16/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TempConverter from './TempConverter';
+
+function App() {
+  return (
+   <div>
+<TempConverter />
+   </div>
+  );
+}
+
+export default App;
---
+++ Assignments/REACT/Preeti/preeti_a16/src/TempBox.js
@@ -0,0 +1,4 @@
+export default TempBox = (props) => {
+    <div/>
+};
+
