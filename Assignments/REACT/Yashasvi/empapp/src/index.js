172e7fe8e17af37fa4e6b8413874803b5910c808
yashubari <bariyashasvi29031997@gmail.com>
2021-04-06 18:08:03 +0530

    made requested changes

--- Assignments/REACT/Yashasvi/empapp/src/index.js
+++ Assignments/REACT/Yashasvi/empapp/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import IcedecApp from './components/icedecApp';
+import CurrencyApp from './components/currencyApp';
+import TempApp from './components/tempApp';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <IcedecApp/>
+    <CurrencyApp/>
+    <TempApp/>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
