27efd860962f00c8f423b341418fa83024162a5b
Priyankashelke2411 <priyankashelke41@gmail.com>
2021-04-07 09:52:18 +0530

    File added :MYSQL assignment 1,2,3

---
+++ Assignments/MYSQL/PriyankaDB1_1.sql
@@ -0,0 +1,73 @@
+show databases;
+USE classicmodels;
+show TABLES;
+#Q1. Display all the fields from employee table.
+SELECT * 
+FROM employees;
+# Q2. Display employee number, first name,last name ,job title of all the employees.
+SELECT employeeNumber,firstName,lastName,jobTitle 
+FROM employees;
+# Q3. Display the office code,phone number,city and country of all offices.
+SELECT officeCode,phone,city,country 
+FROM offices;
+# Q4. Display all the fields from products table.
+SELECT * 
+FROM products;
+#Q5 Display the customer name,phone number,city,country of all the customers.
+SELECT customerName,phone,city,country 
+from customers;
+# Q6 Display the details of customers where the state is null.
+SELECT * 
+FROM customers
+WHERE state = null;
+# Q7 Display the details of customers who are from country France.
+SELECT * 
+FROM customers
+WHERE country = "France";
+# Q8 Display the product code,name,product description and quantity of all the products whose product line is Motocycles.
+SELECT productCode,productName,productDescription,quantityInStock 
+FROM products
+WHERE productLine = 'Motorcycles';
+/*Q 9 Display the product code,name,product description and quantity of all the products whose product line
+is Motocycles and quantity in stock is greater than 6000.*/
+SELECT productCode,productName,productDescription,quantityInStock 
+FROM products
+WHERE productLine = 'Motorcycles'
+AND quantityInStock > 6000;
+# Q10. Display all products whose productline is cars.
+SELECT * 
+FROM products
+WHERE productLine =  'Classic Cars';
+#Q11 Display all the employees who are Sales Rep.
+SELECT * 
+FROM employees
+WHERE jobTitle = 'Sales Rep';
+#12. Display all the employees with office code 4.
+SELECT *
+FROM employees
+WHERE officeCode = 4;
+#13. Display all the employees with office code 1,3 and 4.
+SELECT * 
+FROM employees
+WHERE officeCode IN (1,3,4);
+# Q14. Display all the customers with credit limit between 70000 and 85000.
+SELECT * 
+FROM customers
+WHERE creditLimit >70000
+AND creditLimit <=85000;
+#Q15. Increase the credit limit by 2000 for all and display the customer code,name and credit limit.
+SELECT customerNumber,customerName,creditLimit+2000
+FROM customers;
+#16. Display only those records whose creditlimit after inceasing by 2000 is between 70000 and 90000.
+SELECT  * ,creditLimit+2000
+FROM  customers
+WHERE (creditLimit+2000) >70000
+AND (creditLimit+2000) <85000;
+#USING BETWEEN statement 16. Display only those records whose creditlimit after inceasing by 2000 is between 70000 and 90000.
+SELECT  * ,creditLimit+2000
+FROM  customers
+WHERE creditLimit between 70000 
+AND  85000;
+
+
+
---
+++ Assignments/MYSQL/PriyankaDB3.sql
@@ -0,0 +1,108 @@
+#DML Operations on student and course table
+#Q1. Insert a record in student table
+#Q1_1 Amit 1234567890 9999833445 science 101 camp
+show databases;
+USE classicmodels;
+SELECT * FROM student;
+SELECT * FROM course;
+INSERT INTO student (studID,studName,studAadhar,mobileNo,streams,courseId)VALUES(1,'Amit',1234567895,999983,'science',101);
+/*Q2. Is the record inserted in the table? The record is not inserted because there is a referential integrity
+constraint on courseId and there is no record in the master course table.
+No w try inserting the same record with courseId value as null */
+INSERT INTO student(studID,studName,studAadhar,mobileNo,streams,courseId) VALUES(1,'Amit',123456789,99998,'science',null);
+SELECT * FROM student;
+/*Q3. Insert the following values in course table
+courseID courseName
+101 Computers
+102 Electronics
+103 Mechanical
+104 Civil*/
+SELECT * FROM course;
+INSERT INTO course(courseID,courseName)VALUES(101,'computer');
+INSERT INTO course (courseID,courseName)VALUES(102,'Electronics');
+INSERT INTO course (courseID,courseName)VALUES(103,'Mechanical'); 
+INSERT INTO course (courseID,courseName)VALUES(104,'Civil'); 
+#Q4 Add a column in course table – courseCode varachar(4)
+ALTER TABLE course ADD courseCode VARCHAR(4);
+SELECT * FROM course;
+#Q5. Display all the records in the course table.
+SELECT * FROM course;
+#Q6. Update the courseCode as follows 101 – COMP,102 – ENTC,103- MECH,104 – CIVL
+SET SQL_SAFE_UPDATES = 0;  
+UPDATE course
+SET courseCode =101
+WHERE courseName ='computer';
+UPDATE course
+SET courseCode =102
+WHERE courseName ='Electronics';
+UPDATE course
+SET courseCode =103
+WHERE courseName ='Mechanical';
+UPDATE course
+SET courseCode =104
+WHERE courseName ='Civil';
+SELECT * FROM course;
+#Q7. Insert 10 records in student table against various coourseIds. - use multiple insert. 
+INSERT INTO student(studID,studName,studAadhar,mobileNo,streams,courseId) VALUES(3,'Asmita',1234567,99998,'science',null);
+INSERT INTO student (studID,studName,studAadhar,mobileNo,streams,courseId)VALUES(4,'gita',12345678,9999835,'geograpphy',null);
+INSERT INTO student (studID,studName,studAadhar,mobileNo,streams,courseId)VALUES(5,'sita',1234567891,99998445,'maths',null);
+INSERT INTO student (studID,studName,studAadhar,mobileNo,streams,courseId)VALUES(6,'Anjali',1234567892,9993445,'marathi',null);
+INSERT INTO student (studID,studName,studAadhar,mobileNo,streams,courseId)VALUES(7,'pranjali',12345678921,99833445,'hindi',null);
+INSERT INTO student (studID,studName,studAadhar,mobileNo,streams,courseId)VALUES(8,'swapnali',1234567893,9833445,'english',null);
+INSERT INTO student (studID,studName,studAadhar,mobileNo,streams,courseId)VALUES(9,'manali',1234567895,9999445,'sanskrit',null);
+INSERT INTO student (studID,studName,studAadhar,mobileNo,streams,courseId)VALUES(10,'Neha',12345678951,99833445,'science',null);
+INSERT INTO student (studID,studName,studAadhar,mobileNo,streams,courseId)VALUES(11,'Priyanka',1234567898,9993445,'science',null);
+SELECT * FROM student;
+#Q8. Create a duplicate table of student table. Name it DUP_EMP
+CREATE TABLE DUM_EMP(
+studID INT PRIMARY KEY,
+studName VARCHAR(20)NOT NULL,
+studAadhar VARCHAR(12)UNIQUE,
+mobileNo VARCHAR(8),
+streams VARCHAR(10) CHECK(streams IN ('science','commerce')),
+courseId INT,
+courseName VARCHAR(10));
+DESC DUM_EMP;
+ALTER TABLE DUP_EMP ADD courseName VARCHAR(20);
+#Q9. Delete all the records of course Mechanical course from DUP_EMP table.
+DELETE FROM DUM_EMP WHERE courseName='Mechanical';
+#Q10. set the auotcommit to 0. Give start transaction command.
+SET autocommit=0;
+START TRANSACTION;
+#Q11 Add a columns marks and practical in student table.
+ALTER TABLE student ADD marks Double;
+ALTER TABLE student ADD practical VARCHAR(10);
+#Q12 Give a rollback statement.Check if the columns added are still in the table
+rollback;
+SELECT * FROM student;
+#Q13 insert two more records in student table.
+INSERT INTO student (studID,studName,studAadhar,mobileNo,streams,courseId)VALUES(11,'Amita',123456789,999985,'science',111);
+INSERT INTO student (studID,studName,studAadhar,mobileNo,streams,courseId)VALUES(12,'Anita',1234567891,99945,'history',122);
+#Q14. Give Rollback command. Check if the records are still there in the table.
+rollback;
+SELECT * FROM student;
+#Q15 15. insert two more records in student table.Give a commit command.
+INSERT INTO student (studID,studName,studAadhar,mobileNo,streams,courseId)VALUES(55,'Amit',12345678,999983,'science',null);
+INSERT INTO student (studID,studName,studAadhar,mobileNo,streams,courseId)VALUES(56,'Amita',12345,999983,'science',null);
+commit;
+SELECT * FROM student;
+#Q16. Now give a savepoint A and update the marks and practical marks of all students.
+SET SQL_SAFE_UPDATES=0;
+SAVEPOINT A;
+UPDATE student
+SET marks=90 AND practical=80;
+SELECT * FROM student;
+#Q17. Display the data from student table.See if the data is updated in the table.
+SELECT * FROM student;
+# Q18. Now give savepoint B and delete the records from the student table whose name start from S.
+SAVEPOINT B;
+DELETE FROM student WHERE studName like 's%';
+#Q 19. Display the result and check if the records are deleted.
+SELECT * FROM student;
+#Q 20. Give a Roll back command.Display the results and check if the records are there in the table.
+ROLLBACK;
+SELECT * FROM student;
+
+
+
+
